9d2710c929f0f816f7085a3856d97a8822e0b047
Hsiao Ying <sandra401120422@gmail.com>
2023-11-26 00:54:45 +0800

    Make Added  message interactive

--- script/product.js
+++ script/product.js
@@ -20,21 +20,26 @@ products.forEach((product)=>{
         </div>
 
         <div class="product-quantity-container">
-									<select class="js-quantity-selector-${product.id}">
-										<option selected value="1">1</option>
-										<option value="2">2</option>
-										<option value="3">3</option>
-										<option value="4">4</option>
-										<option value="5">5</option>
-										<option value="6">6</option>
-										<option value="7">7</option>
-										<option value="8">8</option>
-										<option value="9">9</option>
-										<option value="10">10</option>
-									</select>
-								</div>
+						<select class="js-quantity-selector-${product.id}">
+								<option selected value="1">1</option>
+								<option value="2">2</option>
+								<option value="3">3</option>
+								<option value="4">4</option>
+								<option value="5">5</option>
+								<option value="6">6</option>
+								<option value="7">7</option>
+								<option value="8">8</option>
+								<option value="9">9</option>
+								<option value="10">10</option>
+						</select>
+					</div>
 
        <div class="product-spacer"></div>
+
+							<div class="added-to-cart js-added-to-cart-${product.id}">
+									<img src="image/icons/checkmark.png">
+							已加入
+							</div>
 
 							<div class="added-to-cart">
 									<span class="js-btn-added-to cart"></span>
@@ -57,6 +62,7 @@ document.querySelectorAll('.js-add-to-cart')
 		.forEach((button) => {
 				button.addEventListener('click', () => {
 					const productId = button.dataset.productId;
+					// const {productId} = button.dataset(進階寫法);
 
 					let matchingItem;
 
@@ -75,8 +81,10 @@ document.querySelectorAll('.js-add-to-cart')
 						matchingItem.quantity +=quantity;
 					} else{
 							cart.push({
-							productId: productId,
-							quantity:quantity
+						productId: productId,
+            //	productId(進階),
+						quantity:quantity
+							//quantity
 						});
 					}
 					
@@ -89,6 +97,14 @@ document.querySelectorAll('.js-add-to-cart')
 					document.querySelector('.js-cart-quantity')
 						.innerHTML = cartQuantity;
 
+          const addedMessage = document.querySelector(
+            `.js-added-to-cart-${productId}`);
+
+          addedMessage.classList.add('added-to-cart-visible');
+
+          setTimeout(() => {
+            addedMessage.classList.remove('added-to-cart-visible');
+          }, 2000);
 				})
 		})
 
